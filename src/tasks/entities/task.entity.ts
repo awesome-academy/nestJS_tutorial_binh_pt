831ed13f215d60e9d066f34205db958db910663f
pham.thanh.binh <pham.thanh.binh@sun-asterisk.com>
2024-01-19 13:42:52 +0700

    orm

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
---
+++ src/tasks/pipes/custom_validation_res.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, Injectable, ValidationPipe } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
+
+@Injectable()
+export class CustomValidationRes extends ValidationPipe {
+  constructor() {
+    super({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((hash, error) => {
+          hash[error.property] = Object.values(error.constraints);
+          return hash;
+        }, {});
+
+        return new BadRequestException({
+          statusCode: 400,
+          message: I18nContext.current().t("common.validate_error"),
+          errors: formattedErrors,
+        });
+      },
+    });
+  }
+}
